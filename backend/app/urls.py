65998954c7bad192df7a6ff8c41025bda297c9dc
v-adhithyan <aavispeaks@gmail.com>
2023-11-02 12:52:03 +0530

    prod setting changes

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from .views import view_form, list_forms, view_form_data
@@ -7,3 +9,6 @@ urlpatterns = [
     path('form/<uuid:uuid>/data/', view_form_data, name='view_form_data'),
     path('forms/list/', list_forms, name='list_forms'),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- backend/base/prod_settings.py
+++ backend/base/prod_settings.py
@@ -2,3 +2,6 @@ from .settings import *
 
 DEBUG = False
 SECRET_KEY = 'A.d@md@340mdn-2QAQW23??.2232323434ddscs'
+STATIC_ROOT = BASE_DIR / 'static'
+ALLOWED_HOSTS = ['adhithyanvijay.pythonanywhere.com']
+MEDIA_URL = '/media/'
